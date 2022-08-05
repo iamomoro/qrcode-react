47738818b76fb1bc7082541891c8b7eb28b446a3
Elvis Omoro <iamomoroit@gmail.com>
2022-08-05 15:01:29 +0300

    qr-working-download-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import QRCode  from 'qrcode';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState('');
+  const [qrcode, setQrcode] = useState('');
+
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, {
+      
+    }, (err, url) => {
+      if (err) return console.error(err);
+
+      console.log(url);
+      setQrcode(url);
+    });
+  };
 
   return (
-    <div className="App">
-      
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        placeholder=" e.g. https://github.com/"
+        value={url}
+        onChange={(evt) => setUrl(evt.target.value)}
+      />
+      <button onClick={GenerateQRCode}>Generate</button>
+      {qrcode && <>
+         <img src={qrcode} />
+         <a href={qrcode} download="qrcode.png">Download</a>
+      </>
+      }
+   
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
