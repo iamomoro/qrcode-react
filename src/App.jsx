271cd885fbb07a1317737357d2e582bf42f1d441
Elvis Omoro <iamomoroit@gmail.com>
2022-08-05 17:52:26 +0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import QRCode  from 'qrcode';
+import QRCode from 'qrcode';
 import { useState } from 'react';
 
 function App() {
@@ -6,44 +6,47 @@ function App() {
   const [qrcode, setQrcode] = useState('');
 
   const GenerateQRCode = () => {
-    QRCode.toDataURL(url, {
-      width: 800,
-      margin: 2,
-      color: {
-        dark: '#000000ff',
-        light: '#ffffff'
-      }
-    }, (err, url) => {
-      if (err) return console.error(err);
+    QRCode.toDataURL(
+      url,
+      {
+        width: 800,
+        margin: 2,
+        color: {
+          dark: '#000000ff',
+          light: '#ffffff',
+        },
+      },
+      (err, url) => {
+        if (err) return console.error(err);
 
-      setQrcode(url);
-    });
+        setQrcode(url);
+      }
+    );
   };
 
   return (
     <>
-    <div className="app">
-      <h1>QR Code Generator</h1>
-      <input
-        type="text"
-        placeholder=" e.g. https://github.com/"
-        value={url}
-        onChange={(evt) => setUrl(evt.target.value)}
-      />
-      <button onClick={GenerateQRCode}>Generate</button>
-      {qrcode && <>
-         <img src={qrcode} />
-         <a href={qrcode} download="qrcode.png">Download</a>
-      </>
-      }
-    
-    
-   
-    </div>
-    <div className="footer">
-      Made with ❤️ by Elvis Omoro
-    </div>
-</>
+      <div className="app">
+        <h1>QR Code Generator</h1>
+        <input
+          type="text"
+          placeholder=" e.g. https://github.com/"
+          value={url}
+          onChange={(evt) => setUrl(evt.target.value)}
+        />
+        <button onClick={GenerateQRCode}>Generate</button>
+        {qrcode && (
+          <>
+            <img src={qrcode} />
+            <a href={qrcode} download="qrcode.png">
+              Download
+            </a>
+          </>
+        )}
+      </div>
+      <div className="footer">React JS QR Code Generator</div>
+      <div className="footer">Made with ❤️ by Elvis Omoro</div>
+    </>
   );
 }
 
